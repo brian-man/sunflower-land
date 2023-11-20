c60fa2d2f43eeaa73bc108be8395c0723a4cf0cf
Brian Manthos <manthos@msn.com>
2023-11-20 02:14:13 -0800

    Autoplace stone, iron, and gold rocks in INITIAL_RESOURCES

--- src/features/game/lib/landData.ts
+++ src/features/game/lib/landData.ts
@@ -92,6 +92,40 @@ const OFFLINE_FARM_FRUIT = getEnabledNodeCount(
   "Fruit Patch"
 );
 
+function getInitialNodes(name: string) {
+  let count = getEnabledNodeCount(INITIAL_BUMPKIN_LEVEL as BumpkinLevel, name);
+  let x = -1;
+  let y = 9;
+
+  if (INITIAL_EXPANSIONS < 4) {
+    count = name === "Stone Rock" ? 2 : 0;
+    x = 3;
+    y = 7;
+  }
+  if (INITIAL_EXPANSIONS >= 9) {
+    x = -7;
+  }
+
+  if (count === 0) return {};
+
+  if (name === "Iron Rock") x += 1;
+  if (name === "Gold Rock") x += 2;
+
+  return [...Array(count).keys()].reduce(
+    (acc, _, i) => ({
+      ...acc,
+      [i + 1]: {
+        stone: { amount: 1, minedAt: 0 },
+        x: x,
+        y: y - i,
+        height: 1,
+        width: 1,
+      },
+    }),
+    {}
+  );
+}
+
 export const INITIAL_RESOURCES: Pick<
   GameState,
   "crops" | "trees" | "stones" | "iron" | "gold" | "fruitPatches"
@@ -198,31 +232,10 @@ export const INITIAL_RESOURCES: Pick<
       width: 2,
     },
   },
-  stones: {
-    1: {
-      stone: {
-        amount: 1,
-        minedAt: 0,
-      },
-      x: 7,
-      y: 3,
-      height: 2,
-      width: 2,
-    },
-    2: {
-      stone: {
-        amount: 1,
-        minedAt: 0,
-      },
-      x: 3,
-      y: 6,
-      height: 2,
-      width: 2,
-    },
-  },
+  stones: getInitialNodes("Stone Rock"),
+  iron: getInitialNodes("Iron Rock"),
+  gold: getInitialNodes("Gold Rock"),
   fruitPatches: {},
-  gold: {},
-  iron: {},
 };
 
 const INITIAL_BUMPKIN: Bumpkin = {
